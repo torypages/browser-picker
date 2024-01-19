8f0afbdb8f0a8bde4e82e970bf101f059697e7fd
Tory Law <git@torypages.com>
2024-01-19 16:28:05 -0500

    slack changed order, so make it more flexible

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// use std::fs;
+
 fn get_active_window_name() -> String {
     let output = std::process::Command::new("xdotool")
         .arg("getWindowFocus")
@@ -21,9 +23,16 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let url = args.get(1).unwrap();
     let active_window = get_active_window_name();
-    let re = regex::Regex::new(r".*\sTop Hat . Slack$").unwrap();
+    /*
+     * Examples of window titles:
+     *
+     * service-billing - Top Hat - 5 new items - Slack
+     * service-billing - Top Hat - Slack
+     */
+    // fs::write("/tmp/foo", &active_window).expect("Unable to write file");
+    // let re = regex::Regex::new(r"^.*-\sTop\sHat\s-\s([0-9]+\snew\sitem[s]?\s-\s)?Slack$").unwrap();
 
-    if re.is_match(&active_window) {
+    if active_window.contains("Top Hat") && active_window.contains("Slack") {
         launch_browser(&"tophat".to_string(), url);
     } else {
         launch_browser(&"t".to_string(), url);
